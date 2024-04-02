2b3a398e4b8a5eec43ac9920b7fe2c2ef94fd924
lwc05003 <161313798+lwc05003@users.noreply.github.com>
2024-04-02 11:37:44 +0800

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import java.io.BufferedReader;
+import java.io.*;
+/*import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;*/
 
 public class Main {
     public static void main(String[] args)  throws Exception{ 
     /*ONLY FOR WINDOWS MACHINES */
     String filePath = "C:\\Users\\lwc\\Downloads\\Groceries.txt";
+    String fileToWrite = "C:\\Users\\lwc\\Downloads\\ABC.txt";
 
     String [] array = new String[4];
     String id;
@@ -15,26 +19,37 @@ public class Main {
     String separator = ",";
     double total = 0;
 
-    FileReader fileReader = new FileReader(filePath);
-    BufferedReader reader = new BufferedReader(fileReader);
+        FileReader fileReader = new FileReader(filePath);
+         FileWriter fileWriter = new FileWriter(fileToWrite) ;
 
-    String line ;
+        BufferedReader reader = new BufferedReader(fileReader);
+        BufferedWriter writer = new BufferedWriter(fileWriter);
 
-    while((line = reader.readLine()) != null){
+        String line ;
 
-       array = line.split(separator);
+        while((line = reader.readLine()) != null){
 
-       price = Double.parseDouble(array[3]);
+           array = line.split(separator);
 
-       total += price;
-       
-        System.out.println(line);
-     
-       }
-          
-    System.out.println( "\n"+ "Total Price of all items"+Math.round(total));
+           price = Double.parseDouble(array[3]);
 
-    reader.close();
-   
+           total += price;
+           
+            System.out.println(line);
+            writer.write(line);
+            writer.newLine();
+         
+           }
+              
+        System.out.println( "\n"+ "Total Price of all items"+Math.round(total));
+        writer.write("\n"+ "Total Price of groceries"+Math.round(total));
+
+
+
+        reader.close();
+
+        writer.flush();
+        writer.close();
     }
-}
+   
+  }
